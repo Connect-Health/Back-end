78f1c53e2531e760737c579983fd1c811afe1003
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-19 16:02:10 -0300

    Começando projeto

---
+++ connecthealth/src/main/java/br/com/doctordevs/connecthealth/ConnecthealthApplication.java
@@ -0,0 +1,13 @@
+package br.com.doctordevs.connecthealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnecthealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnecthealthApplication.class, args);
+	}
+
+}
---
+++ connecthealth/src/test/java/br/com/doctordevs/connecthealth/ConnecthealthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.doctordevs.connecthealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnecthealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
