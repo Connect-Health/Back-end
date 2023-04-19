cf761ff4d0053cbc1aa1fa3ed7f944985f40de4d
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-19 16:47:03 -0300

    Conexão com o db

--- connecthealth/src/main/java/br/com/doctordevs/connecthealth/Controller.java
+++ connecthealth/src/main/java/br/com/doctordevs/connecthealth/Controller.java
@@ -11,8 +11,12 @@ public class Controller {
 
     private List<Paciente> pacientes = List.of(
         new Paciente("João", "Silva", "11999999999", "1999-01-01", "11111111111", "Rua 1", "joao@gmail", "https://avatars.githubusercontent.com/u/87583563?v=4"),
-        new Paciente("Maria", "Silva", "11999999999", "1999-01-01", "11111111111", "Rua 1", "joao@gmail", "https://avatars.githubusercontent.com/u/125367739?s=64&v=4"),
-        new Paciente("José", "Silva", "11999999999", "1999-01-01", "11111111111", "Rua 1", "joao@gmail", "https://avatars.githubusercontent.com/u/104914975?v=4")
+        new Paciente("Guilherme", "Silva", "11999999999", "1999-01-01", "11111111111", "Rua 1", "joao@gmail", "https://avatars.githubusercontent.com/u/125367739?s=64&v=4"),
+        new Paciente("Rafael", "Silva", "11999999999", "1999-01-01", "11111111111", "Rua 1", "joao@gmail", "https://avatars.githubusercontent.com/u/104914975?v=4")
+    );
+
+    private List<Profissional> profissionais = List.of(
+        new Profissional("João", "Silva", "11999999999", "1999-01-01", "11111111111", "Rua 1", "joao@gmail", "https://avatars.githubusercontent.com/u/87583563?v=4", List.of("Familia", "Depressao", "Ansiedade"), "https://www.google.com", "1h", "R$ 100,00", "Descrição", "Nutrição", "5")
     );
     
     @GetMapping(value = "/pacientes")
@@ -21,7 +25,7 @@ public class Controller {
     }
 
     @GetMapping(value = "/profissionais")
-    public String profissionais() {
-        return "Olá, profissionais!";
+    public List<Profissional> profissionais() {
+        return profissionais;
     }
 }
---
+++ connecthealth/src/main/java/br/com/doctordevs/connecthealth/Profissional.java
@@ -0,0 +1,163 @@
+package br.com.doctordevs.connecthealth;
+
+import java.util.List;
+
+public class Profissional {
+
+    private String nome;
+    private String sobrenome;
+    private String telefone;
+    private String dataNascimento;
+    private String cpf;
+    private String endereco;
+    private String email;
+    private String avatar;
+    private List especialidade;
+    private String certificado;
+    private String duracao;
+    private String preco;
+    private String descricao;
+    private String areaAtuacao;
+    private String avaliacao;
+
+    public Profissional(String nome, String sobrenome, String telefone, String dataNascimento, String cpf,
+            String endereco, String email, String avatar, List especialidade, String certificado, String duracao,
+            String preco, String descricao, String areaAtuacao, String avaliacao) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.email = email;
+        this.avatar = avatar;
+        this.especialidade = especialidade;
+        this.certificado = certificado;
+        this.duracao = duracao;
+        this.preco = preco;
+        this.descricao = descricao;
+        this.areaAtuacao = areaAtuacao;
+        this.avaliacao = avaliacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public List getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(List especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getCertificado() {
+        return certificado;
+    }
+
+    public void setCertificado(String certificado) {
+        this.certificado = certificado;
+    }
+
+    public String getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(String duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getAreaAtuacao() {
+        return areaAtuacao;
+    }
+
+    public void setAreaAtuacao(String areaAtuacao) {
+        this.areaAtuacao = areaAtuacao;
+    }
+
+    public String getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(String avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+}
