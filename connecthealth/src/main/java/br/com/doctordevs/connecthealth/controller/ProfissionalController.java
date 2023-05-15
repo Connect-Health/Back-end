6bfeed82e239c07e5e4e6a830b8cf67a1fe5a31e
wendel <wh68639@gmail.com>
2023-05-15 10:34:14 -0300

    ok

--- connecthealth/src/main/java/br/com/doctordevs/connecthealth/controller/ProfissionalController.java
+++ connecthealth/src/main/java/br/com/doctordevs/connecthealth/controller/ProfissionalController.java
@@ -1,10 +1,13 @@
 package br.com.doctordevs.connecthealth.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.doctordevs.connecthealth.model.Profissional;
@@ -12,7 +15,7 @@ import br.com.doctordevs.connecthealth.service.ProfissionalService;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ProfissionalController {
 
     private final ProfissionalService profissionalService;
@@ -22,11 +25,28 @@ public class ProfissionalController {
     }
 
     @GetMapping("/profissionais")
-    public List<Profissional> listarProfissionais(HttpServletResponse response) {
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        return profissionalService.listarProfissionais();
+public List<Profissional> listarProfissionais(
+        @RequestParam(name = "page", defaultValue = "0") int page,
+        @RequestParam(name = "size", defaultValue = "4") int size,
+        HttpServletResponse response) {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+
+    List<Profissional> profissionais = profissionalService.listarProfissionais();
+
+    int totalPages = (int) Math.ceil((double) profissionais.size() / (double) size);
+    if (page < 0) {
+        page = 0;
+    } else if (page >= totalPages) {
+        page = totalPages - 1;
     }
 
+    List<Profissional> paginatedProfissionais = IntStream.range(page * size, Math.min((page + 1) * size, profissionais.size()))
+            .mapToObj(i -> profissionais.get(i))
+            .collect(Collectors.toList());
+
+    return paginatedProfissionais;
+}
+    
     @GetMapping("/profissionais/nutricionistas")
     public List<Profissional> listarNutricionistas() {
         return profissionalService.buscarNutricionistas();
