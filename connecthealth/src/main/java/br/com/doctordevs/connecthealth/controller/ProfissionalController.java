56696c0d8d3c0f24d35cb891b5c80b2a0d3be14e
wendel <wh68639@gmail.com>
2023-05-19 21:48:39 -0300

    CrossOrigin para todos

--- connecthealth/src/main/java/br/com/doctordevs/connecthealth/controller/ProfissionalController.java
+++ connecthealth/src/main/java/br/com/doctordevs/connecthealth/controller/ProfissionalController.java
@@ -2,6 +2,7 @@ package br.com.doctordevs.connecthealth.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import br.com.doctordevs.connecthealth.service.ProfissionalService;
 
 @RestController
 @RequestMapping("/profissional")
+@CrossOrigin(origins = "*")
 public class ProfissionalController {
 
     @Autowired
