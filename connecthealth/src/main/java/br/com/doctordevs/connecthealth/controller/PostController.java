d0b7085f87185f669d43afc1732bc4c4e732b5b2
Guilherme Gabriel Garcia <125367739+GuilhermeGGarcia12@users.noreply.github.com>
2023-05-27 01:37:35 -0300

    Controller criado

--- connecthealth/src/main/java/br/com/doctordevs/connecthealth/controller/PostController.java
+++ connecthealth/src/main/java/br/com/doctordevs/connecthealth/controller/PostController.java
@@ -1,5 +1,21 @@
 package br.com.doctordevs.connecthealth.controller;
 
+import br.com.doctordevs.connecthealth.model.Post;
+import br.com.doctordevs.connecthealth.service.PostService;
+import java.sql.Connection;
+import java.util.List;
 public class PostController {
-    
+    private PostService postService;
+
+    public PostController(Connection connection) {
+        this.postService = new PostService(connection);
+    }
+
+    public void criarPost(Post post) {
+        postService.createPost(post);
+    }
+
+    public List<Post> obterTodosPosts() {
+        return postService.getAllPosts();
+    }
 }
