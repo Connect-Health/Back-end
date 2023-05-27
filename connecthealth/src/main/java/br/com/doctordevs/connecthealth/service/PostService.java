48fc7ba3208bfd4841d0fd53a4b1fdedb123b8f9
Guilherme Gabriel Garcia <125367739+GuilhermeGGarcia12@users.noreply.github.com>
2023-05-27 01:36:11 -0300

    Post service feito

--- connecthealth/src/main/java/br/com/doctordevs/connecthealth/service/PostService.java
+++ connecthealth/src/main/java/br/com/doctordevs/connecthealth/service/PostService.java
@@ -1,5 +1,32 @@
 package br.com.doctordevs.connecthealth.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public class PostService {
-    
+
+    private PostRepository postRepository;
+
+    public PostService(Connection connection) {
+        this.postRepository = new PostRepository(connection);
+    }
+
+    public void createPost(Post post) {
+        try {
+            postRepository.createPost(post);
+            System.out.println("Post criado com sucesso.");
+        } catch (SQLException e) {
+            System.out.println("Erro ao criar o post: " + e.getMessage());
+        }
+    }
+
+    public List<Post> getAllPosts() {
+        try {
+            return postRepository.getAllPosts();
+        } catch (SQLException e) {
+            System.out.println("Erro ao obter os posts: " + e.getMessage());
+            return null;
+        }
+    }
 }
