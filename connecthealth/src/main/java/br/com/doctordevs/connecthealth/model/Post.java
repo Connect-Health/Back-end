cfba9f2ef06ba9635fd74c9645e4580d23e3f0f1
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-05-28 13:35:20 -0300

    join na tabela post

--- connecthealth/src/main/java/br/com/doctordevs/connecthealth/model/Post.java
+++ connecthealth/src/main/java/br/com/doctordevs/connecthealth/model/Post.java
@@ -1,6 +1,5 @@
 package br.com.doctordevs.connecthealth.model;
 
-import java.sql.Blob;
 import java.sql.Date;
 
 import jakarta.persistence.Column;
@@ -8,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,69 +17,69 @@ public class Post {
 
    @Id
    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int postId;
+   private int postId;
 
-    @Column
-    private String titulo;
+   @Column
+   private String titulo;
 
-    @Column
-    private String conteudo;
+   @Column
+   private String conteudo;
 
-    @Column
-    private String imagem;
+   @Column
+   private String imagem;
 
-    @Column
-    private Date dataPublicacao;
+   @Column
+   private Date dataPublicacao;
 
-    @Column
-    private int profissionalId;
+   @ManyToOne
+   @JoinColumn(name = "profissional_id")
+   private Profissional profissional;
 
-    
    public int getPostId() {
-    return postId;
- }
+      return postId;
+   }
 
- public void setPostId(int postId) {
-    this.postId = postId;
- }
+   public void setPostId(int postId) {
+      this.postId = postId;
+   }
 
- public String getTitulo() {
-    return titulo;
- }
+   public String getTitulo() {
+      return titulo;
+   }
 
- public void setTitulo(String titulo) {
-    this.titulo = titulo;
- }
+   public void setTitulo(String titulo) {
+      this.titulo = titulo;
+   }
 
- public String getConteudo() {
-    return conteudo;
- }
+   public String getConteudo() {
+      return conteudo;
+   }
 
- public void setConteudo(String conteudo) {
-    this.conteudo = conteudo;
- }
+   public void setConteudo(String conteudo) {
+      this.conteudo = conteudo;
+   }
 
- public String getImagem() {
-    return imagem;
- }
+   public String getImagem() {
+      return imagem;
+   }
 
- public void setImagem(String imagem) {
-    this.imagem = imagem;
- }
+   public void setImagem(String imagem) {
+      this.imagem = imagem;
+   }
 
- public Date getDataPublicacao() {
-    return dataPublicacao;
- }
+   public Date getDataPublicacao() {
+      return dataPublicacao;
+   }
 
- public void setDataPublicacao(Date dataPublicacao) {
-    this.dataPublicacao = dataPublicacao;
- }
+   public void setDataPublicacao(Date dataPublicacao) {
+      this.dataPublicacao = dataPublicacao;
+   }
 
- public int getProfissionalId() {
-    return profissionalId;
- }
+   public Profissional getProfissional() {
+      return profissional;
+   }
 
- public void setProfissionalId(int profissionalId) {
-    this.profissionalId = profissionalId;
- }
+   public void setProfissional(Profissional profissional) {
+      this.profissional = profissional;
+   }
 }
