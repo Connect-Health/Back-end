f970caa857d64c9385fb24515e9f923c618dfe10
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-06-17 15:27:17 -0300

    CrossOrigin do chat

--- connecthealth/src/main/java/br/com/doctordevs/connecthealth/controller/ChatController.java
+++ connecthealth/src/main/java/br/com/doctordevs/connecthealth/controller/ChatController.java
@@ -1,10 +1,12 @@
 package br.com.doctordevs.connecthealth.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/chat")
+@CrossOrigin(origins = "*")
 public class ChatController {
     
     @RequestMapping
