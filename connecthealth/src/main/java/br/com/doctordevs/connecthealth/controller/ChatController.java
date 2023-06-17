6fdf399660dd7ae7e5690b377827095330b299cd
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-06-17 18:12:05 -0300

    Mudando para GetMapping

--- connecthealth/src/main/java/br/com/doctordevs/connecthealth/controller/ChatController.java
+++ connecthealth/src/main/java/br/com/doctordevs/connecthealth/controller/ChatController.java
@@ -1,6 +1,7 @@
 package br.com.doctordevs.connecthealth.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class ChatController {
     
-    @RequestMapping
+    @GetMapping
     public String getChat() {
         return "lBIe7pQX3BgKE0EIEyNiT3BlbkFJEQURQib7RSwXhC64Z9Qu";
     }
