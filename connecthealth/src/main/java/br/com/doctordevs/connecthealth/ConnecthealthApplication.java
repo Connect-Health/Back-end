e647a781751fd27bf6931e3b07eeaa3f59f71cf8
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-07-17 16:40:33 -0300

    chore: Atualizar configurações do db no application.properties

--- connecthealth/src/main/java/br/com/doctordevs/connecthealth/ConnecthealthApplication.java
+++ connecthealth/src/main/java/br/com/doctordevs/connecthealth/ConnecthealthApplication.java
@@ -1,5 +1,12 @@
 package br.com.doctordevs.connecthealth;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -7,18 +14,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class ConnecthealthApplication implements CommandLineRunner {
 
+    @Autowired
+    private DataSource dataSource;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class ConnecthealthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConnecthealthApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConnecthealthApplication.class, args);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        try (Connection connection = dataSource.getConnection()) {
+            System.out.println("Conexão com o banco de dados estabelecida!");
+        } catch (SQLException e) {
+            System.out.println("Erro ao tentar estabelecer conexão com o banco de dados!");
+            e.printStackTrace();
+        }
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		return encoder;
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
 }
