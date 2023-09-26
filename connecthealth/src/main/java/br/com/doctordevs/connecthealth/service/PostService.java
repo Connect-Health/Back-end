23b9e42e50bdff54aaf379a6345b5df225e93faa
Wendel Harrison] <wendel.wdhr@gmail.com>
2023-09-26 20:38:17 -0300

    Configurando o MySQL na AWS RDS

--- connecthealth/src/main/java/br/com/doctordevs/connecthealth/service/PostService.java
+++ connecthealth/src/main/java/br/com/doctordevs/connecthealth/service/PostService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.doctordevs.connecthealth.model.Post;
 import br.com.doctordevs.connecthealth.repository.PostRepository;
-import jakarta.transaction.Transactional;
 
 @Service
 public class PostService {
